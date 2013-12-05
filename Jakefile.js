1130801e5c92a0135925e8c1a188ab12fe72c250
Joscha Feth <joscha@feth.com>
2013-12-05 16:34:04 +0100

    Fixes all JSHint problems except for a deliberate switch/case fall through in parse.js.

--- Jakefile.js
+++ Jakefile.js
@@ -15,12 +15,13 @@ task("default", ["build/CSSOM.js"]);
 
 directory("build");
 
-file("build/CSSOM.js", ["src/files.js", "src/CSSOM.js", "build"], function() {
-	var parts = [readFile("src/CSSOM.js")];
+file("build/CSSOM.js", ["src/files.js", "src/CSSOM_START.js", "src/CSSOM_END.js", "build"], function() {
+	var parts = [readFile("src/CSSOM_START.js")];
 	require("./src/files").files.forEach(function(path) {
 		var text = readFile("lib/" + path + ".js");
 		parts.push(stripCommonJS(text).trimLeft());
 	});
+	parts.push(readFile("src/CSSOM_END.js"));
 	FS.writeFileSync("build/CSSOM.js", parts.join(""));
 	process.stdout.write("build/CSSOM.js is done\n");
 });
